646a80201c058137c5eeda619cb792d4447dc132
Erick Jesus <erickontiveros017@gmail.com>
2025-07-21 22:25:18 -0300

    Iniciando projeto

---
+++ src/main/java/com/erickjesus/educ_sys_db/EducSysDbApplication.java
@@ -0,0 +1,13 @@
+package com.erickjesus.educ_sys_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducSysDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducSysDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickjesus/educ_sys_db/EducSysDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickjesus.educ_sys_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducSysDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
